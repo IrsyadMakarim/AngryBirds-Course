5a3a8376e6dd68121f23b2aee755a7477d973fe2
irsyad0141 <1rsy4d.m@gmail.com>
2021-10-11 14:58:57 +0700

    Initial commit

---
+++ Assets/Scripts/EndExplosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndExplosion : MonoBehaviour
+{
+    public Animator ExplosionAnimator;
+    private void Update()
+    {
+        if (ExplosionAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !ExplosionAnimator.IsInTransition(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/OwlBird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    public GameObject Explosion;
+    private bool _hasExploded = false;
+    private void Update()
+    {
+        if(_hasExploded == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Explode()
+    {
+        if (State == BirdState.Thrown && !_hasExploded)
+        {
+            Instantiate(Explosion, transform.position, Quaternion.identity);
+            _hasExploded = true;
+        }
+    }
+
+    public override void OnTap()
+    {
+        Explode();
+    }
+}
